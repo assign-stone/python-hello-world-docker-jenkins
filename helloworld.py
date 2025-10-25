42c3ee7346ce6932a7b809d9d748ebac69d838d2
Shivani Joshi <97306266+assign-stone@users.noreply.github.com>
2025-10-26 03:58:14 +0530

    Update greeting message in Flask app

--- helloworld.py
+++ helloworld.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from Dockerized Python App!"
+    return "Hello, World from Dockerized Flask App!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
